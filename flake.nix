f24ccb15090f564fae3484416669c35dde9ef57b
nix <rwstauffer72@gmail.com>
2023-12-23 10:18:15 -0500

    update flake to potentially bundle dependencies for emacs clients with emacs
    lsp-bridge requires some python dependencies, so hopefully including them as propogated build inputs can prevent me from having to add them to my global shell in home manager. If not its no big deal.

--- flake.nix
+++ flake.nix
@@ -16,9 +16,9 @@
     nixpkgs,
     flake-utils,
     emacs-overlay,
-      
     # emacs-ng,
-  }: flake-utils.lib.eachDefaultSystem (
+  }:
+    flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = import nixpkgs {
           system = system;
@@ -27,8 +27,8 @@
             #emacs-ng.overlays.default
           ]; # gets emacs and package overlays
         };
-      in {
-        packages."emacs" = pkgs.emacsWithPackagesFromUsePackage {
+
+        compiledEmacs = pkgs.emacsWithPackagesFromUsePackage {
           # Your Emacs config file. Org mode babel files are also
           # supported.
           # NB: Config files cannot contain unicode characters, since
@@ -77,23 +77,98 @@
             epkgs.vterm # this supposedly neesd to go here. Maybe due to external library issues?
             epkgs.treesit-grammars.with-all-grammars
             epkgs.leaf
-	    
           ];
         };
-        packages.default = self.packages.${system}."emacs";
-        devShells.default = pkgs.mkShell {
-          packages = [
-            self.packages.${system}."emacs"
-            pkgs.tree-sitter-grammars.tree-sitter-elisp
-            (pkgs.python3.withPackages (p:
+
+        propogatedBuildInputs = [
+          #runtime deps
+          pkgs.tree-sitter-grammars.tree-sitter-elisp
+          (pkgs.python3.withPackages (p:
+            # I think this works for lsp-bridge
               with p; [
                 epc
                 orjson
                 sexpdata
                 six
+                setuptools
                 paramiko
+                rapidfuzz
               ]))
-          ];
+        ];
+        # ] ++ with pkgs.python311Packages [
+        #       epc
+        #       orjson
+        #       sexpdata
+        #       six
+        #       setuptools
+        #       paramiko
+        #       rapidfuzz
+        #     ];
+      in {
+        packages."emacs" = pkgs.stdenv.mkDerivation {
+          name = "emacs";
+          src = pkgs.emacsWithPackagesFromUsePackage {
+            # Your Emacs config file. Org mode babel files are also
+            # supported.
+            # NB: Config files cannot contain unicode characters, since
+            #     they're being parsed in nix, which lacks unicode
+            #     support.
+            # config = ./emacs.org;
+            config = ./emacs.el;
+
+            # Whether to include your config as a default init file.
+            # If being bool, the value of config is used.
+            # Its value can also be a derivation like this if you want to do some
+            # substitution:
+            #   defaultInitFile = pkgs.substituteAll {
+            #     name = "default.el";
+            #     src = ./emacs.el;
+            #     inherit (config.xdg) configHome dataHome;
+            #   };
+            defaultInitFile = true;
+
+            # Package is optional, defaults to pkgs.emacs
+            # package = pkgs.emacsngWRPgtk; #emacs-ng build
+            package = pkgs.emacs-pgtk; # bleeding edge pgtk
+            # package = pkgs.emacs29-pgtk; # stable latest release pgtk
+
+            # By default emacsWithPackagesFromUsePackage will only pull in
+            # packages with `:ensure`, `:ensure t` or `:ensure <package name>`.
+            # Setting `alwaysEnsure` to `true` emulates `use-package-always-ensure`
+            # and pulls in all use-package references not explicitly disabled via
+            # `:ensure nil` or `:disabled`.
+            # Note that this is NOT recommended unless you've actually set
+            # `use-package-always-ensure` to `t` in your config.
+            alwaysEnsure = true;
+
+            # For Org mode babel files, by default only code blocks with
+            # `:tangle yes` are considered. Setting `alwaysTangle` to `true`
+            # will include all code blocks missing the `:tangle` argument,
+            # defaulting it to `yes`.
+            # Note that this is NOT recommended unless you have something like
+            # `#+PROPERTY: header-args:emacs-lisp :tangle yes` in your config,
+            # which defaults `:tangle` to `yes`.
+            # alwaysTangle = true;
+
+            # Optionally provide extra packages not in the configuration file.
+            extraEmacsPackages = epkgs: [
+              #epkgs => pkgs.emacsPackages namespace in nix
+              epkgs.vterm # this supposedly neesd to go here. Maybe due to external library issues?
+              epkgs.treesit-grammars.with-all-grammars
+              epkgs.leaf
+            ];
+          };
+
+          buildPhase = ''
+            mkdir $out
+            cp -r * $out/
+          '';
+          buildInputs = propogatedBuildInputs;
+        };
+
+        packages.default = self.packages.${system}."emacs";
+        devShells.default = pkgs.mkShell {
+          packages = propogatedBuildInputs;
         };
       }
     );
